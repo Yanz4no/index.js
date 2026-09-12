32605d290e465684a1f98531351489689cb2d9f3
Yanz4no <yanyanharyedy0@gmail.com>
2026-09-12 11:47:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-import { patcher } from "@vendetta";
-import { findByProps } from "@vendetta/metro";
+const { patcher, metro } = vendetta;
 
-const UserProfileStore = findByProps("getUserProfile");
-const UserStore = findByProps("getCurrentUser");
+const UserProfileStore = metro.findByProps("getUserProfile");
+const UserStore = metro.findByProps("getCurrentUser");
 
 const GIF_BANNER_URL = "https://cdn.discordapp.com/attachments/1330769136397193250/1548187978109558885/From_Klickpin.com-_Save_these_14_Creative_holiday_table_setting_ideas_that_bring_together_comfort_beauty_and_useful_ideas_you_will_actually_try_fo.gif?ex=6aa625e9&is=6aa4d469&hm=047522f4cee3213814f3f72fa391b2592d3eebabc8e1160a53fc5f31f407a88e&";
 
