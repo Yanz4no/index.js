47053d664bdb63eabdbd83ab9a86148909941016
Yanz4no <yanyanharyedy0@gmail.com>
2026-09-12 11:51:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 (() => {
-  const { patcher, metro } = vendetta;
-
-  const UserProfileStore = metro.findByProps("getUserProfile");
-  const UserStore = metro.findByProps("getCurrentUser");
-
-  const GIF_BANNER_URL = "https://cdn.discordapp.com/attachments/1330769136397193250/1548187978109558885/From_Klickpin.com-_Save_these_14_Creative_holiday_table_setting_ideas_that_bring_together_comfort_beauty_and_useful_ideas_you_will_actually_try_fo.gif?ex=6aa625e9&is=6aa4d469&hm=047522f4cee3213814f3f72fa391b2592d3eebabc8e1160a53fc5f31f407a88e&";
-
   let unpatch;
 
   return {
     onLoad: () => {
-      unpatch = patcher.after("getUserProfile", UserProfileStore, (args, res) => {
-        const currentUser = UserStore?.getCurrentUser();
-        if (currentUser && args[0] === currentUser.id && res) {
-          res.banner = GIF_BANNER_URL;
-          res.bannerURL = GIF_BANNER_URL;
+      try {
+        const v = window.vendetta || vendetta;
+        const UserProfileStore = v.metro.findByProps("getUserProfile");
+        const UserStore = v.metro.findByProps("getCurrentUser");
+
+        const GIF_BANNER_URL = "https://cdn.discordapp.com/attachments/1330769136397193250/1548187978109558885/From_Klickpin.com-_Save_these_14_Creative_holiday_table_setting_ideas_that_bring_together_comfort_beauty_and_useful_ideas_you_will_actually_try_fo.gif?ex=6aa625e9&is=6aa4d469&hm=047522f4cee3213814f3f72fa391b2592d3eebabc8e1160a53fc5f31f407a88e&";
+
+        if (UserProfileStore && UserStore) {
+          unpatch = v.patcher.after("getUserProfile", UserProfileStore, (args, res) => {
+            const currentUser = UserStore.getCurrentUser();
+            if (currentUser && args[0] === currentUser.id && res) {
+              res.banner = GIF_BANNER_URL;
+              res.bannerURL = GIF_BANNER_URL;
+            }
+          });
         }
-      });
+      } catch (err) {
+        console.error("Plugin error:", err);
+      }
     },
     onUnload: () => {
       if (unpatch) unpatch();
